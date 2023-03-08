e51df737504af49e3eb5f6f4d0e724b2797442ed
AndreSierraM <102034926+AndreSierraM@users.noreply.github.com>
2023-03-08 11:27:31 -0500

    git push origin main

--- Parcial1/src/Alumno.java
+++ Parcial1/src/Alumno.java
@@ -1,30 +1,36 @@
 import java.util.Scanner;
+
 public class Alumno {
-    //  creamo los atributos de la clase alumno
-    public String nombre;
-    public String apellido;
-    public int edad;
-    public String identificacion;
-    public String barrio;
-    public String grado;
+    private String nombre;
+    private int edad;
+    private String identificacion;
+    private String barrio;
+    private int grado;
+    private String especialidad;
 
+    public Alumno(String nombre, int edad, String identificacion, String barrio, int grado) {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.identificacion = identificacion;
+        this.barrio = barrio;
+        this.grado = grado;
+    }
 
-    // array de alumnos 
-    // creamos los metodos get y set de la clase alumno
+    public Alumno(String nombre, int edad, String identificacion, String barrio, int grado, String especialidad) {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.identificacion = identificacion;
+        this.barrio = barrio;
+        this.grado = grado;
+        this.especialidad = especialidad;
+    }
+
     public String getNombre() {
         return nombre;
     }
 
     public void setNombre(String nombre) {
         this.nombre = nombre;
-    }
-
-    public String getApellido() {
-        return apellido;
-    }
-
-    public void setApellido(String apellido) {
-        this.apellido = apellido;
     }
 
     public int getEdad() {
@@ -51,93 +57,44 @@ public class Alumno {
         this.barrio = barrio;
     }
 
-    public String getGrado() {
+    public int getGrado() {
         return grado;
     }
 
-    public void setGrado(String grado) {
+    public void setGrado(int grado) {
         this.grado = grado;
-    }
-
-  // creamos el constructor de la clase alumno cuando el alumno tiene una especialidad
-    public Alumno(String nombre, String apellido, int edad, String identificacion, String barrio, String grado, String especialidad) {
-        this.nombre = nombre;
-        this.apellido = apellido;
-        this.edad = edad;
-        this.identificacion = identificacion;
-        this.barrio = barrio;
-        this.grado = grado;
-        this.especialidad = especialidad;
     }
 
     public String getEspecialidad() {
         return especialidad;
     }
 
-    
     public void setEspecialidad(String especialidad) {
         this.especialidad = especialidad;
     }
 
-    public String especialidad;
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
-    // creamos el constructor de la clase alumno cuando el alumno no tiene una especialidad
-    public Alumno(String nombre, String apellido, int edad, String identificacion, String barrio, String grado) {
-        this.nombre = nombre;
-        this.apellido = apellido;
-        this.edad = edad;
-        this.identificacion = identificacion;
-        this.barrio = barrio;
-        this.grado = grado;
-    }
+        int opcion = 0;
+        Alumno[] alumnos = new Alumno[4];
+        int cantidadAlumnos = 0;
 
+        do {
+            System.out.println("MENU");
+            System.out.println("1. Inscribir Alumno");
+            System.out.println("2. Mostrar Datos de los alumnos de un Grado");
+            System.out.println("3. Pagar Matrícula");
+            System.out.println("4. Salir");
 
+            opcion = scanner.nextInt();
 
+            switch(opcion) {
+                case 1:
+                    if (cantidadAlumnos == 4) {
+                        System.out.println("No se pueden inscribir más alumnos");
+                        break;
+                    }
 
-// creamos el metodo imprimir para imprimir los datos del estudiante
+                    System.out.println("Ingrese el nombre del alumno:");
 
-    public void Imprimir() {
-        System.out.println("Nombre: " + nombre);
-        System.out.println("Apellido: " + apellido);
-        System.out.println("Edad: " + edad);
-        System.out.println("Identificacion: " + identificacion);
-        System.out.println("Barrio: " + barrio);
-        System.out.println("Grado: " + grado);
-        System.out.println("Especialidad: " + especialidad);
-    }
- 
-    
-    
-    
-
-
-
-
-  
-
-    // creamos el metodo imprimir_grado para imprimir los datos de los estudiantes de un grado
-    public static void Imprimir_Grado() {
-        
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Ingrese el grado que desea consultar");
-        String grado = sc.next();
-        if (grado.equals("1") || grado.equals("2") || grado.equals("3") || grado.equals("4") || grado.equals("5")) {
-            System.out.println("Los estudiantes del grado " + grado + " son: ");
-            for (int i = 0; i < Main.alumnos.size(); i++) {
-                if (Main.alumnos.get(i).getGrado().equals(grado)) {
-                    Main.alumnos.get(i).Imprimir();
-                }
-            }
-        } else {
-            System.out.println("El grado ingresado no es valido");
-        }
-    }
-
-
-
-
-
-
-
-
-}
