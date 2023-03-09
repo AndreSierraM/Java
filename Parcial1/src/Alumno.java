47e34d9829820007baeb3685ab3fdefe205f175e
AndreSierraM <102034926+AndreSierraM@users.noreply.github.com>
2023-03-08 21:02:12 -0500

    git push origin main

--- Parcial1/src/Alumno.java
+++ Parcial1/src/Alumno.java
@@ -11,10 +11,9 @@ public class Alumno {
     private String barrio;
     private int grado;
     private String especialidad;
-    private boolean matriculaPagada;
 
-    public static void inscribirAlumno(ArrayList<Alumno> listaAlumnos) {
-        Scanner scanner = new Scanner(System.in);
+    public static void inscribirAlumno(ArrayList<Alumno> listaAlumnos, Scanner scanner) {
+        // Verificamos que no se hayan inscrito más de 4 alumnos
 
         if (listaAlumnos.size() >= 4) {
             System.out.println("No se pueden inscribir más alumnos. Ya se han inscrito 4.");
@@ -43,39 +42,40 @@ public class Alumno {
         if (grado >= 6 && grado <= 11) {
             System.out.println("Ingrese la especialidad del alumno (I, E, S, P):");
             especialidad = scanner.nextLine();
+            
         }
 
+        // Creamos el objeto Alumno donde se almacenará la información del alumno
         Alumno alumno;
-        if (grado >= 1 && grado <= 5) {
-            alumno = new Alumno(nombre, edad, identificacion, barrio, grado, "", false);
-        } else if (grado >= 6 && grado <= 11) {
-            alumno = new Alumno(nombre, edad, identificacion, barrio, grado, especialidad, false);
+        if (especialidad.equals("")) {
+            alumno = new Alumno(nombre, edad, identificacion, barrio, grado);
         } else {
-            System.out.println("Grado inválido");
-            return;
+            alumno = new Alumno(nombre, edad, identificacion, barrio, grado, especialidad);
         }
 
-        // Agregamos el nuevo alumno a la lista global de alumnos
+        // imprimimos la lista
+        System.out.println(listaAlumnos);
+
+        // Agregamos el objeto Alumno a la lista de alumnos
         listaAlumnos.add(alumno);
+
+        // imprimimos la lista
+        System.out.println(listaAlumnos);
 
         System.out.println("El alumno ha sido inscrito exitosamente.");
     }
 
-    // Se agregan los atributos que faltan en el constructor
-    public Alumno(String nombre, int edad, int identificacion, String barrio, int grado, String especialidad,
-            boolean matriculaPagada) {
+    public Alumno(String nombre, int edad, int identificacion, String barrio, int grado, String especialidad) {
         this.nombre = nombre;
         this.edad = edad;
         this.identificacion = identificacion;
         this.barrio = barrio;
         this.grado = grado;
         this.especialidad = especialidad;
-        this.matriculaPagada = matriculaPagada;
     }
 
-    // Se agregan los atributos que faltan en el constructor
-    public Alumno(String nombre, int edad, int identificacion, String barrio, int grado, boolean matriculaPagada) {
-        this(nombre, edad, identificacion, barrio, grado, "", matriculaPagada);
+    public Alumno(String nombre, int edad, int identificacion, String barrio, int grado) {
+        this(nombre, edad, identificacion, barrio, grado, "");
     }
 
     // Getters y setters para los atributos de la clase Alumno
@@ -128,15 +128,6 @@ public class Alumno {
         this.especialidad = especialidad;
     }
 
-    public boolean isMatriculaPagada() {
-        return matriculaPagada;
-    }
-
-    public void setMatriculaPagada(boolean matriculaPagada) {
-        this.matriculaPagada = matriculaPagada;
-    }
-
-    // imprimirAlumno
     public void imprimirAlumno() {
         System.out.println("Nombre: " + nombre);
         System.out.println("Edad: " + edad);
@@ -146,108 +137,115 @@ public class Alumno {
         if (grado >= 6 && grado <= 11) {
             System.out.println("Especialidad: " + especialidad);
         }
-        System.out.println("Matricula Pagada: " + matriculaPagada);
 
     }
 
-    // calcularPago
+    public static void calcularPago(ArrayList<Alumno> listaAlumnos) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Ingrese el número de identificación del alumno:");
+        int identificacion = scanner.nextInt();
+        scanner.nextLine();
 
-    public String calcularPago(int alumnoidentifiacionEncontrado, ArrayList<Alumno> listaAlumnos) {
-        // usamos la identificación del alumno para buscar el grado y calcular el valor
-        // de la matrícula  
-        
-        int grado = 0;
- 
-        int valorMatricula = 0;
-        int valorEspecialidad = 0;
-        int valorDerechosGrado = 0;
+        // Buscar el alumno en la lista de alumnos
+        Alumno alumnoEncontrado = null;
         for (Alumno alumno : listaAlumnos) {
-            if (alumno.getIdentificacion() == alumnoidentifiacionEncontrado) {
-                grado = alumno.getGrado();
+            if (alumno.getIdentificacion() == identificacion) {
+                alumnoEncontrado = alumno;
+                break;
             }
-        }  
-
-        System.out.println("El Estudiante " + nombre + " con identificación " + alumnoidentifiacionEncontrado);
-
-        if (grado >= 1 && grado <= 5) {
-            valorMatricula = 650000;
-        } else if (grado >= 6 && grado <= 8) {
-            valorMatricula = 700000;
-            valorEspecialidad = calcularValorEspecialidad();
-        } else if (grado >= 9 && grado <= 10) {
-            valorMatricula = 720000;
-            valorEspecialidad = calcularValorEspecialidad();
-        } else if (grado == 11) {
-            valorMatricula = 800000;
-            valorEspecialidad = calcularValorEspecialidad();
-            valorDerechosGrado = 580000;
-        } else {
-
-            System.out.println("Grado inválido");
-
-             
-        } 
-
-        System.out.println("Valor de la matrícula: " + valorMatricula);
-        if (valorEspecialidad > 0) {
-            System.out.println("Valor de la especialidad: " + valorEspecialidad);
-        }
-        if (valorDerechosGrado > 0) {
-            System.out.println("Valor de los derechos de grado: " + valorDerechosGrado);
-        }
-        valorMatricula += valorEspecialidad + valorDerechosGrado;
-
-        return "Valor total a pagar: " + valorMatricula;
-    } 
-
-    private int calcularValorEspecialidad() {
-        int valorEspecialidad = 0;
-
-        switch (especialidad) {
-            case "I":
-                valorEspecialidad = 500000;
-                break;
-            case "E":
-                valorEspecialidad = 350000;
-                break;
-            case "S":
-                valorEspecialidad = 520000;
-                break;
-            case "P":
-                valorEspecialidad = 400000;
-                break;
-            default:
-                System.out.println("Especialidad inválida");
-                break;
         }
 
-        return valorEspecialidad;
+        // Si el alumno no se encuentra en la lista, se muestra un mensaje de error
+        if (alumnoEncontrado == null) {
+            System.out.println("El alumno no se encuentra inscrito.");
+            return;
+        }
+
+        // Si el alumno se encuentra en la lista, se calcula el valor de la matrícula
+
+        if (alumnoEncontrado != null) {
+            double valorMatricula = 0;
+            double valorEspecialidad = 0;
+            double valorDerechosGrado = 0;
+            int grado = alumnoEncontrado.getGrado();
+            String especialidad = alumnoEncontrado.getEspecialidad();
+
+            // Calcular valor de matrícula
+            if (grado >= 1 && grado <= 5) {
+                valorMatricula = 650000;
+            } else if (grado >= 6 && grado <= 8) {
+                valorMatricula = 700000;
+            } else if (grado >= 9 && grado <= 10) {
+                valorMatricula = 720000;
+            } else if (grado == 11) {
+                valorMatricula = 800000;
+            }
+
+            // Calcular valor de especialidad
+            switch (especialidad) {
+                case "Idiomas":
+                    valorEspecialidad = 500000;
+                    break;
+                case "Electricidad":
+                    valorEspecialidad = 350000;
+                    break;
+                case "Sistemas":
+                    valorEspecialidad = 520000;
+                    break;
+                case "Pedagogía":
+                    valorEspecialidad = 400000;
+                    break;
+                default:
+                    System.out.println("Este alumno no tiene especialida");
+                    break;
+            }
+
+            // Calcular valor de derechos de grado
+            if (grado == 11) {
+                valorDerechosGrado = 580000;
+            }
+
+            // Imprimir resultados
+
+            System.out.println("El Estudiante: " + alumnoEncontrado.getNombre() + " con identificación: "
+                    + alumnoEncontrado.getIdentificacion() + " debe pagar: ");
+            if (grado >= 1 && grado <= 5) {
+                System.out.println("Valor de matrícula: " + valorMatricula);
+            } else if (grado >= 6 && grado <= 10) {
+                System.out.println("Valor de matrícula: " + valorMatricula);
+                System.out.println("Valor de especialidad: " + valorEspecialidad);
+            } else if (grado == 11) {
+                System.out.println("Valor de matrícula: " + valorMatricula);
+                System.out.println("Valor de especialidad: " + valorEspecialidad);
+                System.out.println("Valor de derechos de grado: " + valorDerechosGrado);
+            }
+        }
     }
 
     public static void buscarPorGrado(ArrayList<Alumno> listaAlumnos, int gradoBuscado) {
-
-        boolean encontrado = false;
-        for (Alumno alumno : listaAlumnos) {
-            if (alumno.getGrado() == gradoBuscado) {
-                System.out.println("\nEstudiantes en el grado " + gradoBuscado + ":");
-
-                alumno.imprimirAlumno();
-                encontrado = true;
+        // Buscar los alumnos en la lista de alumnos
+        ArrayList<Alumno> alumnosEncontrados = new ArrayList<>(); // Lista de alumnos encontrados
+        for (Alumno alumno : listaAlumnos) { // Recorrer la lista de alumnos
+            if (alumno.getGrado() == gradoBuscado) { // Si el grado del alumno es igual al grado buscado
+                alumnosEncontrados.add(alumno); // Se agrega el alumno a la lista de alumnos encontrados
             }
         }
-        if (!encontrado) {
-            System.out.println("No se encontraron estudiantes en el grado " + gradoBuscado);
-        }
-    }
 
-    public static Alumno buscarPorIdentificacion(ArrayList<Alumno> listaAlumnos, int identificacionBuscada) {
-        Alumno alumnoidentifiacionEncontrado = null;
-        for (Alumno alumno : listaAlumnos) {
-            if (alumno.getIdentificacion() == identificacionBuscada) {
-                alumnoidentifiacionEncontrado = alumno;
-            }
+        // Si no se encuentran alumnos, se muestra un mensaje de error
+        if (alumnosEncontrados.size() == 0) { // Si la lista de alumnos encontrados está vacía
+            System.out.println("No se encontraron alumnos en el grado " + gradoBuscado); // Se muestra un mensaje de
+                                                                                         // error
+            return;
         }
-        return alumnoidentifiacionEncontrado;
+
+        // Si se encuentran alumnos, se muestran los datos de los alumnos
+        for (Alumno alumno : alumnosEncontrados) {
+            System.out.println("\n Datos del alumno:");
+            System.out.println("\n------------------------\n");
+            alumno.imprimirAlumno();
+            System.out.println("\n------------------------\n");
+        }
+
     }
 
 }
